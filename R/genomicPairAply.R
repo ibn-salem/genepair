c056f26e14063149d7cd24d4c22936a446a35ebf
ibn-salem <jonas.ibn-salem@gmx.de>
2017-01-25 17:51:06 +0100

    add example data set to applyToCisPairs() and draft algorithm for subset of pairs on same chrom

--- R/genomicPairAply.R
+++ R/genomicPairAply.R
@@ -14,17 +14,66 @@
 #'   summary statsitic. Default is \code{\link{cor()}}.
 #' @return A Numeric vector
 #' @examples
-#' gp <- data.frame(g1=c(1,2), g2=c(2,3))
-#' rangesGR <- GRanges(chr1, IRanges(c(100, 200, 300), c(150, 250, 350)))
-#' datamat <- rbind(c(10, 20, 30), c(15, 26, 40), c(100, 2, 0))
-#'
-#' applyToCisPairs(gp, rangesGR, datamat)
-#'
+# rangesGR <- GRanges(
+#  rep(c("chr1", "chr2"), c(3,2)),
+#  IRanges(
+#       c(100, 200, 300, 100, 200),
+#       c(150, 250, 350, 150, 250)
+#    ))
+#
+# datamat <- rbind(
+#     c(10, 20, 30),
+#     c(15, 26, 40),
+#     c(100, 2, 0),
+#     c(10, 20, 30),
+#     c(15, 26, 40)
+#   )
+#
+# applyToCisPairs(gp, rangesGR, datamat)
+#
 #' @keywords internal
 #'
 applyToCisPairs <- function(gp, rangesGR, datamat, fun=cor){
 
+  # Algorithm
+  # (1) group pairs by chromosome
+  # (2) get mapping from single chr index to rangesIdx
+  #     M <- empty matrix with NAs of dim nxn
+  # (3) iterate over all chroms
+  #     - select ranges on this chrom
+  #     - comp corelation matrix of all pairs
+  #     - Fill
+
+  idx1 <- gp[,1]
+  idx2 <- gp[,2]
+
   # to check that paris are from same chromosome
-  chr <- seqnames(rangesGR)
+  chr <- as.vector(seqnames(rangesGR))
+  if(!all(chr[idx1] == chr[idx2])){strop("Pairs of ranges are not all from the same chromosome.")}
+  # check that ranges GR is annotated with a seqinof object
+
+  chrPair <- chr[idx1]
+  # just for testing:
+  #chrPair <- c("chr1", "chr2", "chr1", "chr1", "chr2")
+
+  chromosomes <- unique(chrPair)
+
+  countChr <- plyr::count(chrPair)
+
+  chrID <- rep(NA, length(chrPair))
+
+  for(chr in countChr$x){
+    chrSub <- chr == chrPair
+    chrID[chrSub] <- seq(sum(chrSub))
+  }
+
+  chrIdxList <- lapply(chromosomes, function(chr) which(chr == chrPair))
+
+  vecList <- lapply(chromosomes, function(chr){
+
+    # get subset of matrix
+    m <- datamat[chrIdxList[[chr]],]
+
+  })
 
 }
