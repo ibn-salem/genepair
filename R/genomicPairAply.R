56c691e286dfa3ab02904ddccb60483d4e35cf99
ibn-salem <jonas.ibn-salem@gmx.de>
2017-02-07 20:59:21 +0100

    add draft of applyToClosePairs() function

--- R/genomicPairAply.R
+++ R/genomicPairAply.R
@@ -103,3 +103,99 @@ applyToCisPairs <- function(gp, rangesGR, datamat, fun=cor){
   return(values)
 
 }
+
+
+#' Apply a function to pairs of close genomic regions.
+#'
+#' This function returns a vector with the resulting functin call for each input pair.
+#'
+#'
+#' @param gp data.frame of pairs of genomic ranges. First two columns hold
+#'   indices of the range in \code{rangesGR}.
+#' @param rangesGR GenomicRanges object with all ranges used in \code{gp}.
+#' @param datamat a matrix of with data values associated to each rang in
+#'   \code{rangesGR}. Assumes the ranges in rows while \code{row.names(datamat)}
+#'   are the \code{names(rangesGR)}.
+#' @param fun A function that takes two numeric vectors as imput to compute a
+#'   summary statsitic. Default is \code{\link{cor()}}.
+#' @return A Numeric vector
+#' @examples
+#' rangesGR <- GRanges(
+#'  rep(c("chr1", "chr2"), c(3,2)),
+#'  IRanges(
+#'       c(100, 200, 300, 100, 200),
+#'       c(150, 250, 350, 150, 250)
+#'    ))
+#' gp <- data.frame(
+#'  g1=c(1,4,2,1,4),
+#'  g2=c(2,4,3,3,5)
+#' )
+#'
+#' datamat <- rbind(
+#'     c(10, 20, 30),
+#'     c(15, 26, 40),
+#'     c(100, 2, 0),
+#'     c(10, 20, 30),
+#'     c(15, 26, 40)
+#'   )
+#' applyToCisPairs(gp, rangesGR, datamat)
+#'
+#' @export
+applyToClosePairs <- function(gp, rangesGR, datamat, fun=cor, maxDist=10^6){
+
+  # Algorithm
+  # (1) Group genome in overlapping bins of size 2*maxDist
+  # (2) Run pairwise correlation for all ranges in each bin
+  # (3) Combine correlations to data.frame with proper id1 and id2 in first columns
+  # (4) Query data frame with input pairs
+  #   /uses inner_join() from dplyr like in
+  #  http://stackoverflow.com/questions/26596305/match-two-data-frames-based-on-multiple-columns
+
+  #-----------------------------------------------------------------------------
+  # (1) group ranges in by bins
+  #-----------------------------------------------------------------------------
+
+  # create GRanges object for entire genome
+  genomeGR <- GenomicRanges::GRanges(GenomeInfoDb::seqinfo(rangesGR))
+
+  # tile genoe in overlapping bins of with 2*maxDist
+  binGR <- unlist(GenomicRanges::slidingWindows(genomeGR, 2*maxDist, maxDist))
+
+  hits <- GenomicRanges::findOverlaps(binGR, rangesGR)
+  #-----------------------------------------------------------------------------
+  # (2) compute pairwise correlatin for all ranges in each bin
+  #-----------------------------------------------------------------------------
+  corMatList <- lapply(1:length(binGR), function(i){
+
+    # get regions in this bin
+    idxs <- S4Vectors::subjectHits(hits)[S4Vectors::queryHits(hits) == i]
+
+    n = length(idxs)
+
+    # compute pairwise correlations for all regions in this bin
+    m <- cor(t(datamat[idxs,]))
+
+    corMat <- cbind(
+      rep(idxs, n),
+      rep(idxs, each=n),
+      array(m)
+      )
+  })
+
+  #-----------------------------------------------------------------------------
+  # (3) combine all data.frames
+  #-----------------------------------------------------------------------------
+  corDF <- data.frame(do.call("rbind", corMatList))
+
+  #-----------------------------------------------------------------------------
+  # (4) Query with input pairs
+  #-----------------------------------------------------------------------------
+  names(corDF) <- c("id1", "id2", "val")
+  names(gp)[1:2] <- c("id1", "id2")
+
+  matches <- dplyr::semi_join(corDF, gp, by=c("id1", "id2"))
+
+
+  return(matches$val)
+
+}
