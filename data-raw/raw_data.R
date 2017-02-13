8c9a8754a1915e57c15e8860710a426e4a2b1bc7
ibn-salem <jonas.ibn-salem@gmx.de>
2017-02-13 14:17:30 +0100

    order example dataset of region pairs (loopDF) by indices

--- data-raw/raw_data.R
+++ data-raw/raw_data.R
@@ -10,6 +10,9 @@ load("data-raw/loopPred.v08.Rad21.w1001.b10.datamat.Rdata")
 # remove duplicates
 loopDF <- loopDF[!duplicated(loopDF[,1:2]),]
 
+# sort rows according to first two columns
+loopDF <- loopDF[order(loopDF[,1], loopDF[,2]),]
+
 devtools::use_data(ancGR)
 devtools::use_data(loopDF)
 devtools::use_data(datamat)
