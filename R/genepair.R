b5dbe71fb863946786c643bd3f7aeed4b2f72441
ibn-salem <jonas.ibn-salem@gmx.de>
2017-02-24 17:23:16 +0100

    add genepair.R for package documentation

---
+++ R/genepair.R
@@ -0,0 +1,8 @@
+#'
+#' This is a collection of functions that work on pairs of genes. Thse gene
+#' pairs consists usually of a data.frame that hold indices of genes in an
+#' associate GRanges object (or strings correponding to names in a GRanges
+#' object)
+#'
+NULL
+
--- R/genepairFunctions.R
+++ R/genepairFunctions.R
@@ -1,10 +1,3 @@
-#'
-#' This is a collection of functions that work on pairs of genes. Thse gene
-#' pairs consists usually of a data.frame that hold indices of genes in an
-#' associate GRanges object (or strings correponding to names in a GRanges
-#' object)
-#'
-
 
 #' Get all possible gene pairs within a given distance range as data.frame
 #'
