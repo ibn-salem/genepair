c7b96b59e7db5e4060ecf79910a8aa9f3c5a1987
Jan Krefting <jkreftin@students.uni-mainz.de>
2017-08-06 19:09:30 +0200

    Fixed namespace problem in getPairAsGR(L); wrote test case for the GR method (which had same problem)

--- tests/testthat/test-genepairFunctions.R
+++ tests/testthat/test-genepairFunctions.R
@@ -12,13 +12,22 @@ testGR <- GenomicRanges::GRanges(
   strand=c("+", "-", "+")
 )
 
+testGR2 <- GenomicRanges::GRanges(
+  c("chr1", "chr1", "chr1"),
+  IRanges::IRanges(
+    c(10, 50,  10),
+    c(20, 100, 20)
+  ),
+  strand=c("+", "-", "+")
+)
+
 testGP <- data.frame(
   g1=c(1, 2, 3, 1),
   g2=c(1, 1, 1, 2)
 )
 
 
-test_that("getAllCisPiars() works on example", {
+test_that("getAllCisPairs() works on example", {
   dp <- getAllCisPairs(testGR, maxDist=100)
 
   cp <- getAllCisPairs(testGR, maxDist=10)
@@ -104,10 +113,22 @@ test_that("filterForCisPairs works on example", {
   expect_equal(ncol(cisGP), ncol(testGP))
 })
 
+test_that("getPairAsGR runs correctly on test case", {
+
+  pairGR <- getPairAsGR(testGP, testGR2)
+
+  expect_equal(length(pairGR), nrow(testGP))
+
+  start1 <- start(testGR2[testGP[2, 1]])
+  start2 <- start(testGR2[testGP[2, 2]])
+  val <- ifelse(start1 < start2, start1, start2)
+  expect_equal(start(pairGR[2]), val)
+})
+
 
 test_that("getPairAsGRL runs correctly on test case", {
 
-  pairGRL <- getPairAsGRL(testGP, testGR)
+  pairGRL <- getPairAsGRL(testGP, testGR2)
 
   expect_equal(length(pairGRL), nrow(testGP))
 
